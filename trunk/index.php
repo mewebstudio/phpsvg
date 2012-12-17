b4f6a25716debc07c6863b342b9fc8bdef61f81a
trialforce <trialforce@757f9840-976e-3603-cd01-b5f6919347bc>
2012-12-17 00:09:07 +0000

    Inkscape support, and some minnor corrections. The version 0.8 is not ready yeat.

--- trunk/index.php
+++ trunk/index.php
@@ -2,20 +2,35 @@
 <html>
     <head>
         <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-        <title>phpSVG - Multipurpose SVG edit or create lib</title>
+        <title>phpSVG - Multipurpose SVG create or edit lib</title>
     </head>
     <body>
         <a href="http://phpsvg.nostaljia.eng.br">
-            <img src="../example/resource/phpSVG.svg" style="width:7%; height: 7%; float:left;"/>
+            <img src="example/resource/phpSVG.svg" style="width:70px;height:30px;float:left;"/>
+            <h1>phpSVG</h1>
         </a>
-        <h1>phpSVG</h1>
-        <p>Lib version:&nbsp;<?php echo file_get_contents('svglib/VERSION'); ?> <a href="svglib/CHANGELOG">Changelog</a><p>
         <p>
-            <a href='http://www.w3.org/TR/SVG/'>SVG</a> means <strong>Scalable Vector graphics</strong>, it a XML based, open-source vector graphics  <br/>
-        supported by various drawings programs like <a href="http://inkscape.org/">Inkscape</a>, Corel Draw and Adobe Illustrator.</p>
-        <p>PhpSVG is a multipurpose library to create or edit SVG using Object Oriented <a href="http://php.net/">PHP</a> scripts.<br/>
-        It's possible to edit a existent SVG document or create a new from scratch.</p>
-        <p>If you don't see the phpSVG logo you browsser don't have SVG view capabilities.</p>
+            Lib version:&nbsp;<?php echo file_get_contents('svglib/VERSION'); ?> <a href="svglib/CHANGELOG">Changelog</a><p>
+        <p>
+            <a href='http://www.w3.org/TR/SVG/'>SVG</a> means <strong>Scalable Vector graphics</strong>, it a XML based, open-source vector graphics supported by various drawings programs like <a href="http://inkscape.org/">Inkscape</a>, Corel Draw and Adobe Illustrator.
+        </p>
+        
+        <p>
+            PhpSVG is a multipurpose library to create or edit SVG using Object Oriented <a href="http://php.net/">PHP</a> scripts.
+            It's possible to edit a existent SVG document or create a new from scratch.
+        </p>
+        
+        <p><i>If you don't see the phpSVG logo you browsser don't have SVG view capabilities.</i></p>
+        
+        <p><strong>Pay attention:</strong><p>
+        <p>This libs dependes on:</p>
+        <p>
+            <a href="http://php.net/manual/en/class.simplexmlelement.php">SimpleXmlElement</a>
+            <a href="http://php.net/manual/en/book.zlib.php">Gzip support</a>
+            <a href="http://php.net/manual/en/book.imagick.php">Imagemagick extension<a/>
+            <a href="http://php.net/manual/pt_BR/book.image.php">PHP-GD</a>
+        </p>
+                
         <p><strong>Links</strong></p>
         <a href='doc/html/index.html'>Documentation.</a>&nbsp;
         <a href='http://code.google.com/p/phpsvg/'>Source code.</a>&nbsp;
@@ -26,13 +41,17 @@
         <?php
         if ( is_array( $examples = glob('example/*.php') ) )
         {
-            foreach ( $examples as $line => $info )
+            foreach ( $examples as $info )
             {
                 $name = basename($info);
                 echo "\n        <a href='$info'>$name</a>&nbsp; - <a href='viewExample.php?example={$name}'>View source</a><br/>";
             }
         }
         ?>
+        
+        <p>If you have some questions, mail me at
+            <a href="mailto:trialforce@gmail.com">trialforce@gmail.com</a>
+        </p>
         
     </body>
 </html>
